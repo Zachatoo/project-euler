936caf56a50a9fd8c6189981f2ea10fdd7d3249e
Zachatoo <Zachatoo@users.noreply.github.com>
2021-04-03 19:10:59 -0600

    Add more problems

--- src/constants/problems.js
+++ src/constants/problems.js
@@ -1,4 +1,5 @@
 export const problems = [
+  // Problem 1
   {
     key: 1,
     title: 'Multiples of 3 and 5',
@@ -18,6 +19,7 @@ for (var i = 1; i < 1000; ++i)
 return sum;`,
     completed: true,
   },
+  // Problem 2
   {
     key: 2,
     title: 'Even Fibonacci numbers',
@@ -42,6 +44,7 @@ while (nextNum <= maxNum)
 return sum;`,
     completed: true,
   },
+  // Problem 3
   {
     key: 3,
     title: 'Largest prime factor',
@@ -77,6 +80,58 @@ for (var i = targetNum / 2; i >= 3; --i)
     }
   }
 }`,
+    completed: false,
+  },
+  // Problem 4
+  {
+    key: 4,
+    title: 'Largest palindrome product',
+    prompt:
+      <>
+        <p>A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.</p>
+        <p>Find the largest palindrome made from the product of two 3-digit numbers.</p>
+      </>,
+    codeStringified: null,
+    completed: false,
+  },
+  // Problem 5
+  {
+    key: 5,
+    title: 'Smallest multiple',
+    prompt:
+      <>
+        <p>2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.</p>
+        <p>What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?</p>
+      </>,
+    codeStringified: null,
+    completed: false,
+  },
+  // Problem 6
+  {
+    key: 6,
+    title: 'Sum square difference',
+    prompt:
+      <>
+        <p>The sum of the squares of the first ten natural numbers is,</p>
+        <p className="text-center">1^2 + 2^2 + ... + 10^2 = 385</p>
+        <p>The square of the sum of the first ten natural numbers is,</p>
+        <p className="text-center">(1 + 2 + ... + 10)^2 = 55^2 = 3025</p>
+        <p>Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is <b>3025 - 385 = 2640.</b></p>
+        <p>Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.</p>
+      </>,
+    codeStringified: null,
+    completed: false,
+  },
+  // Problem 7
+  {
+    key: 7,
+    title: '10001st prime',
+    prompt:
+      <>
+        <p>By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.</p>
+        <p>What is the 10 001st prime number?</p>
+      </>,
+    codeStringified: null,
     completed: false,
   },
 ];
